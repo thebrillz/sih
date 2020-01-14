2331c4934b419d209ab5a1f6c7a96ce17156f2dd
Shambhavi <andorawiles@gmail.com>
2020-01-14 18:33:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sih/Hiring.java
@@ -0,0 +1,14 @@
+package com.example.sih;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hiring extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiring);
+    }
+}
---
+++ app/src/main/java/com/example/sih/PersonalityQuiz.java
@@ -0,0 +1,14 @@
+package com.example.sih;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalityQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality_quiz);
+    }
+}
